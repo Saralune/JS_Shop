f2242059b8e717022371441bb3c6061df8c814a1
DuffauJC <jeancharles.duffau@gmail.com>
2022-05-05 14:50:07 +0200

    push 1 article

--- main.js
+++ main.js
@@ -1,52 +1,27 @@
 
-// recup des données
-
-const data="./data/art.json"
-let  articles= []
-
-fetch(data)
-.then(response => {
-   return response.json();
-})
-.then(jsondata => {
-    //console.log(jsondata.rows)
-
- 
-    jsondata.rows.forEach(el => {
-
-         articles.push(el)
-
-        console.log(el)
-    });
-    
-});
-
-    
-    console.log(Object.entries(articles))
-
- console.log(articles)
-// console.log (articles)
-
 
 //constructeur de l'objet Article
 
-// class Article {
+class Article {
 
-//     constructor(idArticle, description,brand,unitaryPrice, catName, descriptionCat, imageUrl) {
-//         this.idArticle=idArticle;
-//         this.description=description;
-//         this.brand=brand;
-//         this.unitaryPrice=unitaryPrice;
-//         this.catName=catName;
-//         this.descriptionCat=descriptionCat;
-//         this.imageUrl=imageUrl
-//     }
+    constructor(idArticle, description,brand,unitaryPrice, catName, descriptionCat, imageUrl) {
+        this.idArticle=idArticle;
+        this.description=description;
+        this.brand=brand;
+        this.unitaryPrice=unitaryPrice;
+        this.catName=catName;
+        this.descriptionCat=descriptionCat;
+        this.imageUrl=imageUrl
+    }
 
 
-// }
+}
+
 // //Construction des articles
-// let article1 = new Article (1, "Souris","Logitech",85,"Materiel info",
-// "Tout matériel informatique physique en lien avec un ordinateur","img/souris.jpg")
+let article1 = new Article (1, "Souris","Logitech",85,"Materiel info",
+"Tout matériel informatique physique en lien avec un ordinateur","img/souris.jpg")
+let article2 = new Article()
+
 
 // //Récupération des balises pour l'affichage
 let id1=document.getElementById("id1")
@@ -55,23 +30,17 @@ let id3= document.getElementById("id3")
 let id4=document.getElementById("id4")
 let id5=document.getElementById("id5")
 let id6= document.getElementById("id6")
-//let id7=document.getElementById("id7").src=articles[0].ImageUrl
+let id7=document.getElementById("id7").src=article1.imageUrl
 
 
 
 // //Affichage des données 
-// id1.innerHTML=articles[0]
-// id2.innerHTML=articles[0].Description
-// id3.innerHTML=articles[0].Brand
-// id4.innerHTML=articles[0].UnitaryPrice
-// id5.innerHTML=articles[0].CatName
-// id6.innerHTML=articles[0].Descriptions
-
-
-
-
-
-
+id1.innerHTML=article1.idArticle
+id2.innerHTML=article1.description
+id3.innerHTML=article1.brand
+id4.innerHTML=article1.unitaryPrice
+id5.innerHTML=article1.catName
+id6.innerHTML = article1.descriptionCat
 
 
 //Initialisation du local storage (panier)
